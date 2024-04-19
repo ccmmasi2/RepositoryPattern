dfa1ed05689acd3b509717b7473fa8cbf68c2562
cmontenegro <cmontenegro>
2024-04-19 16:08:42 -0500

    basic repository pattern

---
+++ Pattern.Repository.Core/Pattern.Repository.API/Program.cs
@@ -0,0 +1,42 @@
+using Pattern.Repository.API.Services;
+using Pattern.Repository.Core.BaseRepository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ProductService>();
+
+builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(SqlServerRepository<>));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Pattern.Repository.Core/Pattern.Repository.Core/BaseRepository/SqlServerRepository.cs
@@ -0,0 +1,30 @@
+﻿namespace Pattern.Repository.Core.BaseRepository
+{
+    public class SqlServerRepository<T> : IBaseRepository<T> where T : class
+    {
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
